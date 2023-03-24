ea7e8991665039a105f7e55fde7c7cfaaefcd9d0
aaronzipp <aaronzipp@protonmail.com>
2023-03-24 15:09:59 +0100

    Added test using hypothesis module

--- tests/example_utils_test.py
+++ tests/example_utils_test.py
@@ -1,4 +1,13 @@
+from hypothesis import given
+from hypothesis.strategies import integers
+
 from src.example_utils import sum
+
+
+@given(integers(), integers())
+def test_sum_hypothesis(a, b):
+    """Docstring for test_sum_hypothesis function."""
+    assert sum(a, b) == a + b
 
 
 def test_sum():
